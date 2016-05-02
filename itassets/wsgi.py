1144b012798cdb9a032dec4b8f8147cde5702d32
ASI <asi@dpaw.wa.gov.au>
2016-05-02 15:56:18 +0800

    Add dj-static to request, configure wsgi.py properly.

--- itassets/wsgi.py
+++ itassets/wsgi.py
@@ -6,4 +6,7 @@ import confy
 confy.read_environment_file('.env')
 
 from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+from dj_static import Cling
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "itassets.settings")
+application = Cling(get_wsgi_application())
