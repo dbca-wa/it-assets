6b0d44db6134e3b3cf06b1df41c79bfed42a63ca
Ashley Felton <ashley@ropable.com>
2018-05-23 15:03:48 +0800

    Add gunicorn to req's, update manage.py.

--- manage.py
+++ manage.py
@@ -3,7 +3,10 @@ import confy
 import os
 import sys
 
-confy.read_environment_file()
+# These lines are required for interoperability between local and container environments.
+dot_env = os.path.join(os.getcwd(), '.env')
+if os.path.exists(dot_env):
+    confy.read_environment_file()
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "itassets.settings")
