f939307c7ed3b038c1e740640a7c62f0b4a7c95a
Ashley Felton <ashley@ropable.com>
2018-05-24 08:39:46 +0800

    Use dj-static to serve media.

--- itassets/wsgi.py
+++ itassets/wsgi.py
@@ -12,8 +12,8 @@ if os.path.exists(dot_env):
     confy.read_environment_file(dot_env)  # Must precede dj_static imports.
 
 from django.core.wsgi import get_wsgi_application
-from dj_static import Cling
+from dj_static import Cling, MediaCling
 
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'itassets.settings')
-application = Cling(get_wsgi_application())
+application = Cling(MediaCling(get_wsgi_application()))
