ffd728f0f4da7269f1d4b4cf1733bd20fb102248
Ashley Felton <ashley@ropable.com>
2018-08-28 09:12:03 +0800

    Comment unused import in cache_freshdesk_tickets mgmt command.

--- tracking/management/commands/cache_freshdesk_tickets.py
+++ tracking/management/commands/cache_freshdesk_tickets.py
@@ -1,6 +1,6 @@
 from django.core.management.base import BaseCommand
 from django.conf import settings
-import json
+#import json
 import logging
 import requests
 from tracking import utils_freshdesk
