35dee9b0ac36cf588507beaa3d44fad9bbe0f4cf
Ashley Felton <ashley@ropable.com>
2018-10-15 10:56:06 +0800

    Added basic Incident list view.

--- registers/urls.py
+++ registers/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import IncidentDetail
+from .views import IncidentList, IncidentDetail
 
 urlpatterns = [
+    path('incidents/', IncidentList.as_view(), name='incident_list'),
     path('incidents/<int:pk>/', IncidentDetail.as_view(), name='incident_detail'),
 ]
--- registers/views.py
+++ registers/views.py
@@ -1,5 +1,15 @@
-from django.views.generic import DetailView
+from django.views.generic import ListView, DetailView
 from .models import Incident
+
+
+class IncidentList(ListView):
+    paginate_by = 20
+
+    def get_queryset(self):
+        # By default, return ongoing incidents only.
+        if 'all' in self.request.GET:
+            return Incident.objects.all()
+        return Incident.objects.filter(resolution__isnull=True)
 
 
 class IncidentDetail(DetailView):
