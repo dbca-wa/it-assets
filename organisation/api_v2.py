420f7d8a94f53a9bbd791a4da7e0a33b3d2ef1f1
Scott Percival <scott.percival@dbca.wa.gov.au>
2018-12-27 15:52:58 +0800

    update frontend build, merge in changes

--- organisation/api_v2.py
+++ organisation/api_v2.py
@@ -5,7 +5,7 @@ from rest_framework import viewsets, serializers, status, generics, views
 from rest_framework.decorators import detail_route, list_route, renderer_classes, authentication_classes, permission_classes
 from rest_framework_recursive.fields import RecursiveField
 
-from organisation.models import Location, OrgUnit, DepartmentUser
+from organisation.models import Location, OrgUnit, DepartmentUser, CostCentre
 
 
 class UserLocationSerializer(serializers.ModelSerializer):
@@ -111,3 +111,14 @@ class OrgTreeSerializer(serializers.ModelSerializer):
 class OrgTreeViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = OrgUnit.objects.filter(active=True, parent__isnull=True)
     serializer_class = OrgTreeSerializer
+
+
+class CostCentreSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CostCentre
+        fields = ('name', 'code', 'chart_acct_name', 'manager', 'business_manager', 'admin', 'tech_contact')
+
+
+class CostCentreViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = CostCentre.objects.filter(active=True)
+    serializer_class = CostCentreSerializer
