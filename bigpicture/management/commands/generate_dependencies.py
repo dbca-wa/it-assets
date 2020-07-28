4728c5a977d8a30ad3fc87241730704028a12907
Ashley Felton <ashley@ropable.com>
2020-07-28 08:13:46 +0800

    Remove generate/update functions from try/except block to fail loudly.

--- bigpicture/management/commands/generate_dependencies.py
+++ bigpicture/management/commands/generate_dependencies.py
@@ -1,6 +1,6 @@
 from django.core.management.base import BaseCommand, CommandError
-import traceback
 from bigpicture.utils import (
+    audit_risks,
     audit_dependencies,
     webserver_dependencies,
     workload_dependencies,
@@ -28,25 +28,22 @@ class Command(BaseCommand):
     help = 'Creates & updates dependencies and risk assessments from scan data'
 
     def handle(self, *args, **options):
-        try:
-            print('Creating & updating system dependencies and risk assessments')
-            print('Auditing existing dependency objects')
-            audit_dependencies()
-            print('Creating/updating web server dependencies from Nginx proxy rules')
-            webserver_dependencies()
-            print('Creating/updating Kubernetes workload dependencies')
-            workload_dependencies()
-            print('Creating/updating host dependencies')
-            host_dependencies()
-            print('Creating/updating risk assessments for hosts')
-            host_risk_assessment_vulns()
-            print('Creating/updating risk assessments for k3s workloads')
-            workload_risk_assessment_vulns()
-            print('Creating/updating risk assessments for IT systems')
-            itsystem_risks()
-            print('Creating/updating risk assessments for host operating systems')
-            host_os_risks()
-            print('Complete')
-        except:
-            raise CommandError('Error during generation of dependencies & risk assessments')
-            traceback.print_exception()
+        self.stdout.write('Creating & updating system dependencies and risk assessments')
+        self.stdout.write('Auditing existing risk and dependency objects')
+        audit_risks()
+        audit_dependencies()
+        self.stdout.write('Creating/updating web server dependencies from Nginx proxy rules')
+        webserver_dependencies()
+        self.stdout.write('Creating/updating Kubernetes workload dependencies')
+        workload_dependencies()
+        self.stdout.write('Creating/updating host dependencies')
+        host_dependencies()
+        self.stdout.write('Creating/updating risk assessments for hosts')
+        host_risk_assessment_vulns()
+        self.stdout.write('Creating/updating risk assessments for k3s workloads')
+        workload_risk_assessment_vulns()
+        self.stdout.write('Creating/updating risk assessments for IT systems')
+        itsystem_risks()
+        self.stdout.write('Creating/updating risk assessments for host operating systems')
+        host_os_risks()
+        self.stdout.write('Complete')
