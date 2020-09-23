16dc6beb4a07720181b831e834776012519796f3
Ashley Felton <ashley@ropable.com>
2020-09-23 10:02:38 +0800

    Removed al management commands from tracking app (deprecated, unused).

--- tracking/management/commands/pdq_load_data.py
+++
@@ -1,9 +0,0 @@
-from django.core.management.base import BaseCommand
-from tracking.utils_pdq import pdq_load_computers
-
-
-class Command(BaseCommand):
-    help = 'Loads data from PDQ Inventory scans.'
-
-    def handle(self, *args, **options):
-        pdq_load_computers()
