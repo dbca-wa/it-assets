615ce10678307e9dd4fec6c60ab056fe4ed71ae1
Ashley Felton <ashley@ropable.com>
2020-10-19 09:07:33 +0800

    Refactor signal_sciences_extract_feed script to use timestamp and duration.

--- bigpicture/management/commands/signal_sciences_upload_feed.py
+++ bigpicture/management/commands/signal_sciences_upload_feed.py
@@ -4,13 +4,23 @@ from bigpicture import utils
 
 
 class Command(BaseCommand):
-    help = 'Extracts the Signal Science feed for the previous hour and upload it to Azure container'
+    help = '''Extracts the Signal Science feed for the defined duration and upload it to blob storage.
+    Defaults to querying the most-recent 15 minutes of data (offset by a 5 minute delay).
+    '''
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '--minutes-ago', action='store', dest='min_ago', type=int, default=20,
+            help='Extract feed from this many minutes in the past (integer, optional)')
+        parser.add_argument(
+            '--duration', action='store', dest='duration', type=int, default=15,
+            help='Extract feed for this many minutes duration (integer, optional)')
 
     def handle(self, *args, **options):
-        from_time = datetime.utcnow().replace(minute=0, second=0, microsecond=0) - timedelta(hours=1)
-        self.stdout.write('Extracting Signal Sciences feed starting from {}'.format(from_time.isoformat()))
-        filename = utils.signal_sciences_write_feed()
+        from_datetime = datetime.utcnow().replace(second=0, microsecond=0) - timedelta(minutes=options['min_ago'])
+        self.stdout.write('Extracting Signal Sciences feed starting from {}'.format(from_datetime.isoformat()))
+        filename = utils.signal_sciences_write_feed(from_datetime=from_datetime, minutes=options['duration'])
         if filename:
             self.stdout.write(self.style.SUCCESS('{} uploaded to Azure container'.format(filename)))
         else:
-            self.stdout.write(self.style.ERROR('Upload failed'))
+            self.stdout.write(self.style.ERROR('Feed querying and upload failed'))
