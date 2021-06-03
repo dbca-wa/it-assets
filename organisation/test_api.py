d230d61397b1ac4f0fee9699af1598916033952b
Ashley Felton <ashley@ropable.com>
2021-06-03 08:21:50 +0800

    Remove API basic auth unit tests.

--- organisation/test_api.py
+++ organisation/test_api.py
@@ -1,14 +1,8 @@
-import base64
 from django.urls import reverse
 from mixer.backend.django import mixer
 
 from itassets.test_api import ApiTestCase
 from organisation.models import Location, OrgUnit
-
-BASICAUTH_USERS_OVERRIDE = {'testuser': 'pass'}
-credentials = 'testuser:pass'
-b64_credentials = base64.b64encode(credentials.encode())
-AUTH_HEADERS = {'HTTP_AUTHORIZATION': 'Basic {}'.format(b64_credentials.decode())}
 
 
 class DepartmentUserAPIResourceTestCase(ApiTestCase):
@@ -39,21 +33,6 @@ class DepartmentUserAPIResourceTestCase(ApiTestCase):
         self.assertContains(response, self.user1.email)
         self.assertNotContains(response, self.user2.email)
 
-    def test_basic_auth(self):
-        """Test the DepartmentUserAPIResource basic auth handling
-        """
-        url = reverse('department_user_api_resource')
-        response = self.client.get(url)
-        self.assertEqual(response.status_code, 200)
-        self.client.logout()
-        response = self.client.get(url)
-        self.assertEqual(response.status_code, 401)
-
-        # Override settings.BASICAUTH_USERS to put testuser in the dict.
-        with self.settings(BASICAUTH_USERS=BASICAUTH_USERS_OVERRIDE):
-            response = self.client.get(url, follow=True, **AUTH_HEADERS)
-            self.assertEqual(response.status_code, 200)
-
 
 class LocationAPIResourceTestCase(ApiTestCase):
 
@@ -80,21 +59,6 @@ class LocationAPIResourceTestCase(ApiTestCase):
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, self.loc1.name)
         self.assertNotContains(response, self.loc2.name)
-
-    def test_basic_auth(self):
-        """Test the LocationAPIResource basic auth handling
-        """
-        url = reverse('location_api_resource')
-        response = self.client.get(url)
-        self.assertEqual(response.status_code, 200)
-        self.client.logout()
-        response = self.client.get(url)
-        self.assertEqual(response.status_code, 401)
-
-        # Override settings.BASICAUTH_USERS to put testuser in the dict.
-        with self.settings(BASICAUTH_USERS=BASICAUTH_USERS_OVERRIDE):
-            response = self.client.get(url, follow=True, **AUTH_HEADERS)
-            self.assertEqual(response.status_code, 200)
 
 
 class OrgUnitAPIResourceTestCase(ApiTestCase):
@@ -136,21 +100,6 @@ class OrgUnitAPIResourceTestCase(ApiTestCase):
         self.assertContains(response, self.branch1.name)
         self.assertNotContains(response, self.branch2.name)
 
-    def test_basic_auth(self):
-        """Test the OrgUnitAPIResource basic auth handling
-        """
-        url = reverse('orgunit_api_resource')
-        response = self.client.get(url)
-        self.assertEqual(response.status_code, 200)
-        self.client.logout()
-        response = self.client.get(url)
-        self.assertEqual(response.status_code, 401)
-
-        # Override settings.BASICAUTH_USERS to put testuser in the dict.
-        with self.settings(BASICAUTH_USERS=BASICAUTH_USERS_OVERRIDE):
-            response = self.client.get(url, follow=True, **AUTH_HEADERS)
-            self.assertEqual(response.status_code, 200)
-
 
 class LicenseAPIResourceTestCase(ApiTestCase):
 
@@ -180,18 +129,3 @@ class LicenseAPIResourceTestCase(ApiTestCase):
         self.assertContains(response, self.user1.email)
         self.assertNotContains(response, self.user2.email)
         self.assertNotContains(response, self.contractor.email)
-
-    def test_basic_auth(self):
-        """Test the LicenseAPIResource basic auth handling
-        """
-        url = reverse('license_api_resource')
-        response = self.client.get(url)
-        self.assertEqual(response.status_code, 200)
-        self.client.logout()
-        response = self.client.get(url)
-        self.assertEqual(response.status_code, 401)
-
-        # Override settings.BASICAUTH_USERS to put testuser in the dict.
-        with self.settings(BASICAUTH_USERS=BASICAUTH_USERS_OVERRIDE):
-            response = self.client.get(url, follow=True, **AUTH_HEADERS)
-            self.assertEqual(response.status_code, 200)
