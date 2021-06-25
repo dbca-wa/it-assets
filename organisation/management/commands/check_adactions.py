a673a13bdcc0920720b42295b6f5c9c077025ebd
Ashley Felton <ashley@ropable.com>
2021-06-25 11:25:02 +0800

    Simplify check_adactions command (AD data is now cached on the DepartmentUser).

--- organisation/management/commands/check_adactions.py
+++ organisation/management/commands/check_adactions.py
@@ -1,43 +1,15 @@
 from django.core.management.base import BaseCommand
 from organisation.models import DepartmentUser, ADAction
-from organisation.utils import get_ad_users_json, find_user_in_list
 
 
 class Command(BaseCommand):
     help = 'Checks all non-completed AD actions and deletes those no longer required'
 
-    def add_arguments(self, parser):
-        parser.add_argument(
-            '--container',
-            action='store',
-            dest='container',
-            required=True,
-            help='Azure container name'
-        )
-        parser.add_argument(
-            '--json_path',
-            action='store',
-            dest='json_path',
-            required=True,
-            help='JSON output file path'
-        )
-
     def handle(self, *args, **options):
         self.stdout.write(self.style.SUCCESS('Checking all non-completed AD actions'))
-        ad_users = get_ad_users_json(container=options['container'], azure_json_path=options['json_path'])
         user_pks = ADAction.objects.filter(completed__isnull=True).values_list('department_user', flat=True).distinct()
 
         for user in DepartmentUser.objects.filter(pk__in=user_pks):
-            azure_user = find_user_in_list(ad_users, user.email)
-            if azure_user:
-                try:
-                    user.audit_ad_actions(azure_user)
-                except Exception as ex:
-                    self.stdout.write(
-                        self.style.ERROR(
-                            'Exception raised during checking of AD actions for {}'.format(user.email)
-                        )
-                    )
-                    self.stdout.write(self.style.ERROR(ex))
+            user.audit_ad_actions()
 
         self.stdout.write(self.style.SUCCESS('Completed'))
