e5384ba49e0f51ecdabf6b4dac559190573f66bf
Ashley Felton <ashley@ropable.com>
2021-09-07 13:29:33 +0800

    Removed v2 API classes from registers (deprecated).

--- itassets/api_v2.py
+++ itassets/api_v2.py
@@ -1,6 +1,5 @@
 from rest_framework import routers
 from organisation.api_v2 import DepartmentUserViewSet, DepartmentTreeViewSet, LocationViewSet, OrgUnitViewSet, CostCentreViewSet
-from registers.api_v2 import StandardChangeViewSet, ChangeRequestViewSet
 
 
 api_v2_router = routers.DefaultRouter()
@@ -9,5 +8,3 @@ api_v2_router.register(r'departmentuser', DepartmentUserViewSet)
 api_v2_router.register(r'location', LocationViewSet)
 api_v2_router.register(r'orgunit', OrgUnitViewSet)
 api_v2_router.register(r'costcentre', CostCentreViewSet)
-api_v2_router.register(r'changerequest', ChangeRequestViewSet)
-api_v2_router.register(r'standardchange', StandardChangeViewSet)
