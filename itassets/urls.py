1edc14d66aac0f814630ea2e81124952dab6900f
Ashley Felton <ashley@ropable.com>
2021-11-22 07:29:20 +0800

    Remove old healthcheck view.

--- itassets/urls.py
+++ itassets/urls.py
@@ -5,7 +5,6 @@ from django.contrib import admin
 from itassets.api_v1 import urlpatterns as api_v1_urlpatterns
 from itassets.api_v3 import urlpatterns as api_v3_urlpatterns
 from itassets.api import urlpatterns as api_urlpatterns
-from itassets.views import HealthCheckView
 from rancher import urls as rancher_urls
 from registers import urls as registers_urls
 from assets import urls as assets_urls
@@ -26,7 +25,6 @@ urlpatterns = [
     path('rancher/', include(rancher_urls)),
     path('registers/', include(registers_urls)),
     path('organisation/', include(organisation_urls)),
-    path('healthcheck/', HealthCheckView.as_view(), name='health_check'),
     path('markdownx/', include('markdownx.urls')),
     path('favicon.ico', RedirectView.as_view(url='{}favicon.ico'.format(settings.STATIC_URL)), name='favicon'),
     path('', RedirectView.as_view(url='/admin')),
