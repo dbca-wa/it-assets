c5aed1563440e4fa9f3aff8eed1d9b0e8f2a8f51
Ashley Felton <ashley@ropable.com>
2021-12-16 09:41:09 +0800

    Add employee ID to Department User export.

--- organisation/reports.py
+++ organisation/reports.py
@@ -18,8 +18,9 @@ def department_user_export(fileobj, users):
     ) as workbook:
         users_sheet = workbook.add_worksheet('Department users')
         users_sheet.write_row('A1', (
-            'NAME', 'EMAIL', 'TITLE', 'ACCOUNT TYPE', 'EMPLOYMENT STATUS', 'COST CENTRE', 'CC MANAGER', 'CC MANAGER EMAIL', 'CC BMANAGER', 'CC BMANAGER EMAIL',
-            'ACTIVE', 'O365 LICENCE', 'TELEPHONE', 'MOBILE PHONE', 'LOCATION', 'ORGANISATION UNIT', 'GROUP UNIT',
+            'NAME', 'EMAIL', 'TITLE', 'ACCOUNT TYPE', 'EMPLOYEE ID', 'EMPLOYMENT STATUS', 'COST CENTRE',
+            'CC MANAGER', 'CC MANAGER EMAIL', 'CC BMANAGER', 'CC BMANAGER EMAIL', 'ACTIVE', 'M365 LICENCE',
+            'TELEPHONE', 'MOBILE PHONE', 'LOCATION', 'ORGANISATION UNIT', 'GROUP UNIT',
         ))
         row = 1
         for i in users:
@@ -28,6 +29,7 @@ def department_user_export(fileobj, users):
                 i.email,
                 i.title,
                 i.get_account_type_display(),
+                i.employee_id,
                 i.get_employment_status(),
                 i.cost_centre.code if i.cost_centre else '',
                 i.cost_centre.manager.get_full_name() if i.cost_centre and i.cost_centre.manager else '',
@@ -44,15 +46,17 @@ def department_user_export(fileobj, users):
             ])
             row += 1
         users_sheet.set_column('A:A', 35)
-        users_sheet.set_column('B:E', 45)
-        users_sheet.set_column('F:F', 13)
-        users_sheet.set_column('G:G', 35)
-        users_sheet.set_column('H:H', 45)
-        users_sheet.set_column('I:I', 35)
-        users_sheet.set_column('J:J', 45)
-        users_sheet.set_column('K:L', 13)
-        users_sheet.set_column('M:N', 20)
-        users_sheet.set_column('O:Q', 60)
+        users_sheet.set_column('B:D', 45)
+        users_sheet.set_column('E:E', 12)
+        users_sheet.set_column('F:F', 45)
+        users_sheet.set_column('G:G', 13)
+        users_sheet.set_column('H:H', 35)
+        users_sheet.set_column('I:I', 45)
+        users_sheet.set_column('J:J', 35)
+        users_sheet.set_column('K:K', 45)
+        users_sheet.set_column('L:M', 13)
+        users_sheet.set_column('N:O', 20)
+        users_sheet.set_column('P:R', 60)
 
     return fileobj
 
