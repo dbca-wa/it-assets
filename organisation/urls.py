2ed789e82b8e5f0cbb12fe5bcb5348d4c3b4540d
Ashley Felton <ashley@ropable.com>
2022-05-03 13:25:43 +0800

    Remove ADAction views (deprecated).

--- organisation/urls.py
+++ organisation/urls.py
@@ -5,7 +5,4 @@ urlpatterns = [
     path('address-book/', views.AddressBook.as_view(), name='address_book'),
     path('user-accounts/', views.UserAccounts.as_view(), name='user_accounts'),
     path('user-accounts/export/', views.UserAccountsExport.as_view(), name='user_accounts_export'),
-    path('adaction/', views.ADActionList.as_view(), name='ad_action_list'),
-    path('adaction/<int:pk>/', views.ADActionDetail.as_view(), name='ad_action_detail'),
-    path('adaction/<int:pk>/complete/', views.ADActionComplete.as_view(), name='ad_action_complete'),
 ]
