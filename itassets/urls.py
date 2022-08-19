5784d87957df420dc729046dc0636bea720f9908
Ashley Felton <ashley@ropable.com>
2022-08-19 13:50:11 +0800

    Deprecate v1 API endpoints, remove restless.

--- itassets/urls.py
+++ itassets/urls.py
@@ -2,9 +2,7 @@ from django.conf import settings
 from django.urls import path, include
 from django.views.generic import RedirectView
 from django.contrib import admin
-from itassets.api_v1 import urlpatterns as api_v1_urlpatterns
 from itassets.api_v3 import urlpatterns as api_v3_urlpatterns
-from itassets.api import urlpatterns as api_urlpatterns
 from rancher import urls as rancher_urls
 from registers import urls as registers_urls
 from organisation import urls as organisation_urls
@@ -18,8 +16,6 @@ admin.site.site_title = 'IT Assets'
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v3/', include(api_v3_urlpatterns)),
-    path('api/v1/', include(api_v1_urlpatterns)),
-    path('api/', include(api_urlpatterns)),
     path('rancher/', include(rancher_urls)),
     path('registers/', include(registers_urls)),
     path('organisation/', include(organisation_urls)),
