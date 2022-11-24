e10c587657497b2e29a081ee6ae333858814e1f7
Ashley Felton <ashley@ropable.com>
2022-11-24 14:35:25 +0800

    Hide the Site app from the Django admin.

--- itassets/urls.py
+++ itassets/urls.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.urls import path, include
 from django.views.generic import RedirectView
 from django.contrib import admin
+from django.contrib.sites.models import Site
 from itassets.api_v3 import urlpatterns as api_v3_urlpatterns
 from rancher import urls as rancher_urls
 from registers import urls as registers_urls
@@ -11,6 +12,7 @@ from organisation import urls as organisation_urls
 admin.site.site_header = 'IT Assets database administration'
 admin.site.index_title = 'IT Assets database'
 admin.site.site_title = 'IT Assets'
+admin.site.unregister(Site)
 
 
 urlpatterns = [
